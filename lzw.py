08398ba157213bc2a0e80778a6c3cd0fbe24126b
Italo <italo.oliveira300@hotmail.com>
2021-09-01 00:56:04 -0300

    feat: add decompressor and refactor code

---
+++ lzw.py
@@ -0,0 +1,78 @@
+from typing import List
+from math import ceil
+
+class LZW:
+
+    def __init__(self) -> None:
+        pass
+    
+
+    def compressor(self, text) -> List[int]:
+        last_idx = 128
+        c_dict = {}
+        acc = ''
+        output_encoded = []
+
+        for idx, word in enumerate(text):
+
+            if self.in_dict(acc + word, c_dict):
+                acc += word
+            else:
+                code = self.get_code_from_dict(acc, c_dict)
+                output_encoded.append(code)
+                
+                c_dict[acc + word] = last_idx
+                last_idx += 1
+
+                acc = word
+        
+        code = self.get_code_from_dict(acc, c_dict)
+        output_encoded.append(code)
+
+        return output_encoded
+
+
+    def decompressor(self, list_encode) -> str:
+        c_dict = {}
+        last_idx = 128
+        next_code = list_encode[0]
+        result = self.translate(next_code, c_dict)
+        old_code = next_code
+
+        for code in list_encode[1:]:
+            if code in c_dict or code < 128:
+                actual_word = self.translate(code, c_dict)
+                
+                result += actual_word
+                acc = self.translate(old_code, c_dict)
+
+                c_dict[last_idx] = acc + actual_word[0]
+                last_idx += 1
+            else:
+                acc = self.translate(old_code, c_dict)
+                new_char = acc + acc[0]
+                result += new_char
+
+                c_dict[last_idx] = new_char
+                last_idx += 1
+            
+            old_code = code
+        
+        return result
+
+
+    def translate(self, code, c_dict):
+        return chr(code) if code < 128 else c_dict[code]
+
+
+    def in_dict(self, word, dict):
+        try:
+            return word in dict or ord(word) < 128
+        except:
+            return False
+
+
+    def get_code_from_dict(self, word, c_dict):
+        return c_dict[word] if word in c_dict else ord(word)
+
+
--- main.py
+++ main.py
@@ -1,40 +1,6 @@
 from math import ceil
+from lzw import LZW
 
-def get_next_char(i, text):
-    return text[i+1] if i + 1 < len(text) else ''
-
-def compressor(text):
-    last_index = 128
-    c_dict = {}
-    output_encod = []
-    p = ''
-    c = ''
-
-    for i, word in enumerate(text):
-        if i == 0:
-            p = word
-            
-        c = get_next_char(i, text)
-        acc = p + c
-
-        if acc in c_dict:
-            p = acc
-        else:
-            print(word)
-            if p in c_dict:
-                output_encod.append(c_dict[p])
-            else:
-                output_encod.append(ord(p))
-
-            c_dict[acc] = last_index
-            last_index += 1
-            p = c
-    
-    print(c_dict)
-    print(output_encod)
-    print(len(output_encod))
-
-    return output_encod
 
 def open_txt():
     with open('tests/01.txt', 'r') as f:
@@ -42,16 +8,34 @@ def open_txt():
     return content
 
 def save(list_encode):
-    with open('t1.bin', 'wb') as f:
+    with open('t1.bin', 'wb') as f:        
+        # Get needed bytes to max value in list
+        max_value = max(list_encode)
+        num_bytes = needed_bytes(max_value)
+
+        # Writing needed bytes in first position of file
+        f.write(to_byte(num_bytes))
+
+        # Writing values in list as binary
         for num_code in list_encode:
             f.write(to_byte(num_code))
 
-def to_byte(num):
+def needed_bytes(num):
     b = bin(num)[2:]
-    return int.to_bytes(num, ceil(len(b) / 8), 'big')
+    return ceil(len(b) / 8)
+
+def to_byte(num):
+    return int.to_bytes(num, needed_bytes(num), 'big')
+
+
+
+# Main
 
 content = open_txt()
-print(content)
 
-list_encode = compressor(content)
+lzw = LZW()
+
+list_encode = lzw.compressor(content)
 save(list_encode)
+
+print(lzw.decompressor(list_encode))
