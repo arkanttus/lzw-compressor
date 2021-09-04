dc5652a71d9fb27039d55c8ab0186db6e5a34096
Italo <italo.oliveira300@hotmail.com>
2021-09-03 23:21:45 -0300

    refactor: improvement readability code

--- file_utils.py
+++ file_utils.py
@@ -1,9 +1,15 @@
+from math import ceil
 
+"""
+Return needed bytes to represent a number
+"""
 def needed_bytes(num):
     b = bin(num)[2:]
     return ceil(len(b) / 8)
 
-
+"""
+Return a number in bytes
+"""
 def to_byte(num, num_bytes):
     return int.to_bytes(num, num_bytes, 'big')
 
@@ -13,13 +19,13 @@ def open_txt(file_to_compress):
         content = f.read()
     return content
 
-
+"""
+Open a bin file and convert to a list of integers
+"""
 def open_bin(file):
     with open(file, 'rb') as file:
         byte_length = int.from_bytes(file.read(1), byteorder='big')
-        # file.seek(1)
-        # print(byte_length)
-        arr = list()
+        arr = []
 
         while True:
             byte = file.read(byte_length)
@@ -31,8 +37,10 @@ def open_bin(file):
 
         return arr
 
-
-def save_bin(list_encode):
+"""
+Receive a list of integers, transform this list in bytes and save into file
+"""
+def save_bin(list_encode, path_save_bin_file):
     with open(path_save_bin_file, 'wb') as f:
         # Get needed bytes to max value in list
         max_value = max(list_encode)
